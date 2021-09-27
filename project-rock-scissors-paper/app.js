c24462fac4a14ed0b11bfaaed6f06a643b64051f
Lia <sriayuliani1514@gmail.com>
2021-09-27 15:02:18 +0800

    rock-scissors-paper

---
+++ project-rock-scissors-paper/app.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+// TODO: Require the http module
+const http = require('http')
+
+// TODO: Create a server
+const server = http.createServer((request, response) => {
+    // TODO: Create a url object with request url and host name
+    const url = new URL(request.url, 'http://${request.headers.host}')
+    const { method } = request
+    //  switch statement based on pathname of url
+    switch (url.pathname) {
+        case '/':
+            if (method === 'GET') {
+                // Get value of 'name' query
+                let name = url.searchParams.get('name')
+                console.log(name)
+                // Write response header
+                response.writeHeader(200, { "Content-Type": "text/html" });
+                // Pipe index.html to response
+                fs.createReadStream('index.html').pipe(response)
+                break
+            }
+            else if (method === 'POST') {
+                handlePostResponse(request, response)
+                break
+            }
+        default:
+            response.writeHead(404, { "Content-Type": "text/html" })
+            fs.createReadStream("404.html").pipe(response)
+    }
+})
+
+server.listen(4001, () => {
+    console.log(`server listening ${server.address().port}`)
+})
+
+// Function for handling POST responses
+function handlePostResponse(request, response) {
+    request.setEncoding('utf8');
+
+    // Receive chunks on 'data' event and concatenate to body variable
+    let body = '';
+    request.on('data', function (chunk) {
+        body += chunk;
+    });
+
+    // When done receiving data, select a random choice for server
+    // Compare server choice with player's choice and send an appropriate message back
+    request.on('end', function () {
+        const choices = ['rock', 'paper', 'scissors'];
+        const randomChoice = choices[Math.floor(Math.random() * 3)];
+
+        const choice = body;
+
+        let message;
+
+        const tied = `Aww, we tied! I also chose ${randomChoice}.`;
+        const victory = `Dang it, you won! I chose ${randomChoice}.`;
+        const defeat = `Ha! You lost. I chose ${randomChoice}.`;
+
+        if (choice === randomChoice) {
+            message = tied;
+        } else if (
+            (choice === 'rock' && randomChoice === 'paper') ||
+            (choice === 'paper' && randomChoice === 'scissors') ||
+            (choice === 'scissors' && randomChoice === 'rock')
+        ) {
+            message = defeat;
+        } else {
+            message = victory;
+        }
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end(`You selected ${choice}. ${message}`);
+    });
+}
